3b349910ccb16fb45cc3cd5302cdacdbc83f7ed2
FXLP <1399774043@qq.com>
2019-12-22 02:13:29 +0800

    add back-to-top

--- src/views/specification/newSpecification.vue
+++ src/views/specification/newSpecification.vue
@@ -1,67 +1,70 @@
 <template>
-  <el-backtop target=".page-component__scroll .el-scrollbar__wrap">
-    <div class="app-container">
-      <el-form ref="dynamicValidateForm" :model="dynamicValidateForm" label-width="400px" class="demo-dynamic">
-        <el-form-item
-          label="规范名称"
-          prop="specificationName"
-          style="width:1200px"
-          :rules="[
-            { required: true, message: '请输入标注规范名称', trigger: 'blur' },
-            { min: 1, max: 10, message: '长度在 1 到 15 个字符', trigger: 'blur' }
-          ]"
-        >
-          <el-input v-model="dynamicValidateForm.specificationName" />
-        </el-form-item>
-        <el-form-item
-          label="上传标注规范文件"
-          prop="specificationFile"
-          style="width:1200px"
-          :rules="{
-            required: true, message: '规范文件不能为空，请上传', trigger: 'blur'
-          }"
-        >
-          <el-input v-model="dynamicValidateForm.specificationFile" />
-        </el-form-item>
-        <el-form-item label="创建实体集合元组" style="width:1200px">
-          <el-button circle size="mini" type="primary" @click="newEntityGroup()"><i class="el-icon-plus" /></el-button>
-        </el-form-item>
-        <el-form-item
-          v-for="(entityGroup, index) in dynamicValidateForm.entityGroups"
-          :key="entityGroup.key"
-          :label="'实体集'+(index+1)"
-          :prop="'entityGroups.'+index+'.name'"
-          :rules="{
-            required: true, message: '实体集名不能为空', trigger: 'blur'
-          }"
-        >
-          <el-input v-model="entityGroup.name" style="width:800px" /><el-button type="danger" size="mini" icon="el-icon-delete" circle @click.prevent="removeEntityGroup(entityGroup)" />
-        </el-form-item>
-        <el-form-item label="创建关系集合" style="width:1000px">
-          <el-button circle size="mini" type="primary" @click="newRelationship()"><i class="el-icon-plus" /></el-button>
-        </el-form-item>
-        <el-form-item
-          v-for="(relationship, index) in dynamicValidateForm.relationships"
-          :key="relationship.key"
-          :label="'关系名'+(index+1)"
-          :prop="'relationships.'+index+'.Rname'"
-          :rules="{
-            required: true, message: '关系名不能为空', trigger: 'blur'
-          }"
-        >
-          <el-input v-model="relationship.Rname" style="width:800px" /><el-button type="danger" size="mini" icon="el-icon-delete" circle @click.prevent="removeRelationship(relationship)" />
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="submitForm('dynamicValidateForm')">立即创建</el-button>
-          <el-button @click="resetForm('dynamicValidateForm')">重置</el-button>
-        </el-form-item>
-      </el-form>
-    </div>
-  </el-backtop>
+  <div class="app-container">
+    <el-form ref="dynamicValidateForm" :model="dynamicValidateForm" label-width="400px" class="demo-dynamic">
+      <el-form-item
+        label="规范名称"
+        prop="specificationName"
+        style="width:1200px"
+        :rules="[
+          { required: true, message: '请输入标注规范名称', trigger: 'blur' },
+          { min: 1, max: 10, message: '长度在 1 到 15 个字符', trigger: 'blur' }
+        ]"
+      >
+        <el-input v-model="dynamicValidateForm.specificationName" />
+      </el-form-item>
+      <el-form-item
+        label="上传标注规范文件"
+        prop="specificationFile"
+        style="width:1200px"
+        :rules="{
+          required: true, message: '规范文件不能为空，请上传', trigger: 'blur'
+        }"
+      >
+        <el-input v-model="dynamicValidateForm.specificationFile" />
+      </el-form-item>
+      <el-form-item label="创建实体集合元组" style="width:1200px">
+        <el-button circle size="mini" type="primary" @click="newEntityGroup()"><i class="el-icon-plus" /></el-button>
+      </el-form-item>
+      <el-form-item
+        v-for="(entityGroup, index) in dynamicValidateForm.entityGroups"
+        :key="entityGroup.key"
+        :label="'实体集'+(index+1)"
+        :prop="'entityGroups.'+index+'.name'"
+        :rules="{
+          required: true, message: '实体集名不能为空', trigger: 'blur'
+        }"
+      >
+        <el-input v-model="entityGroup.name" style="width:800px" /><el-button type="danger" size="mini" icon="el-icon-delete" circle @click.prevent="removeEntityGroup(entityGroup)" />
+      </el-form-item>
+      <el-form-item label="创建关系集合" style="width:1000px">
+        <el-button circle size="mini" type="primary" @click="newRelationship()"><i class="el-icon-plus" /></el-button>
+      </el-form-item>
+      <el-form-item
+        v-for="(relationship, index) in dynamicValidateForm.relationships"
+        :key="relationship.key"
+        :label="'关系名'+(index+1)"
+        :prop="'relationships.'+index+'.Rname'"
+        :rules="{
+          required: true, message: '关系名不能为空', trigger: 'blur'
+        }"
+      >
+        <el-input v-model="relationship.Rname" style="width:800px" /><el-button type="danger" size="mini" icon="el-icon-delete" circle @click.prevent="removeRelationship(relationship)" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('dynamicValidateForm')">立即创建</el-button>
+        <el-button @click="resetForm('dynamicValidateForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+    <el-tooltip placement="top" content="回到顶部">
+      <back-to-top :custom-style="myBackToTopStyle" :visibility-height="300" :back-position="50" transition-name="fade" />
+    </el-tooltip>
+  </div>
 </template>
 
 <script>
+import BackToTop from '@/components/BackToTop'
 export default {
+  components: { BackToTop },
   data() {
     return {
       dynamicValidateForm: {
@@ -77,6 +80,15 @@ export default {
             Rname: ''
           }
         ]
+      },
+      myBackToTopStyle: {
+        right: '50px',
+        bottom: '50px',
+        width: '40px',
+        height: '40px',
+        'border-radius': '4px',
+        'line-height': '45px', // 请保持与高度一致以垂直居中 Please keep consistent with height to center vertically
+        background: '#e7eaf1'// 按钮的背景颜色 The background color of the button
       }
     }
   },
